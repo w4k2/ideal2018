4012276ec76babb9932c6b001812f8e97de0dceb
Paweł <xehivs@mini.local>
2018-08-01 15:06:51 +0200

    True learning.

--- method.py
+++ method.py
@@ -5,6 +5,7 @@ Hamming and others.
 """
 import numpy as np
 import helper as h
+from sklearn import metrics
 
 
 class FeatureSelectingEnsemble():
@@ -14,7 +15,8 @@ class FeatureSelectingEnsemble():
     Base structure for genetic algorithm.
     """
 
-    def __init__(self, alpha=.5, beta=.5, n_members=5, p=.5):
+    def __init__(self, base_clf, alpha=.5, beta=.5, n_members=5,
+                 p=.5, parents=None):
         """
         Initializer.
 
@@ -24,27 +26,53 @@ class FeatureSelectingEnsemble():
         self.beta = beta
         self.n_members = n_members
         self.p = p
+        self.parents = parents
+        self.base_clf = base_clf
 
     def fit(self, X, y):
         """Compute model."""
+        self.X = X
+        self.y = y
         self.d = X.shape[1]
-        self.selected_features = np.zeros((self.n_members,
-                                           self.d)).astype(bool)
-        self.selected_features = np.random.choice(a=[False, True],
-                                                  size=(self.n_members,
-                                                        self.d),
-                                                  p=[1-self.p, self.p])
+        if not hasattr(self, 'selected_features'):
+            # Random features if none established
+            self.selected_features = np.zeros((self.n_members,
+                                               self.d)).astype(bool)
+            self.selected_features = np.random.choice(a=[False, True],
+                                                      size=(self.n_members,
+                                                            self.d),
+                                                      p=[1-self.p, self.p])
+        # Establishing ensemble
         self.ensemble = []
+        for features_mask in self.selected_features:
+            X_ = self.X[:, features_mask]
+            clf = self.base_clf()
+            clf.fit(X_, y)
+            self.ensemble.append(clf)
 
-    def bac(self):
+    def bac(self, X, y):
         """Balanced accuracy score."""
-        return .85
+        # Calculate ensemble support matrix
+        esm = []
+        for i, clf in enumerate(self.ensemble):
+            sv = clf.predict_proba(X[:, self.selected_features[i]])
+            esm.append(sv)
+        esm = np.array(esm)
 
-    def quality(self):
+        # Calculate support as mean for member classifiers
+        mesm = np.mean(esm, axis=0)
+
+        # Prediction and score
+        prediction = np.argmax(mesm, axis=1)
+        score = metrics.balanced_accuracy_score(y, prediction)
+
+        return score
+
+    def quality(self, X, y):
         """Optimization criteria."""
         a = self.alpha * self.features_proportion()
         b = self.beta * self.average_hamming()
-        return self.bac() - a - b
+        return self.bac(X, y) - a + b
 
     def average_hamming(self):
         """Return average hamming measure between members of commitee."""
--- process.py
+++ process.py
@@ -7,9 +7,11 @@ techniques.
 import numpy as np
 import method as m
 import helper as h
-from sklearn import svm
+from sklearn import naive_bayes
 
 np.random.seed(2)
+
+base_clf = naive_bayes.GaussianNB
 
 datasets = h.datasets_for_groups([
     "imb_IRhigherThan9p1",
@@ -22,13 +24,12 @@ for dataset in datasets:
     X, y, X_, y_ = h.load_dataset(dataset)
     print(X.shape)
 
-    fse = m.FeatureSelectingEnsemble(p=.5)
+    fse = m.FeatureSelectingEnsemble(base_clf=base_clf)
     fse.fit(X, y)
 
     print(fse.average_hamming())
     print(fse.features_proportion())
-    print(fse.bac())
-    print(fse.quality())
+    print(fse.quality(X, y))
     exit()
 
 base_clf = svm.SVC()
